68b51fcfa47edfce9a631c244aa27586a47276cb
Jun Lei <jlei91@gmail.com>
2021-04-16 13:15:02 -0700

    first commit

---
+++ src/main/java/com/fitnessnews/FitnessNewsSbApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessNewsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessNewsSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnessnews/FitnessNewsSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessNewsSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
